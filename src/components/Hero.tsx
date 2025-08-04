df33eb3030bae9f18b595709ee9a9443b38c69be
shawncasaus <shawn.casaus@gmail.com>
2025-08-04 18:41:10 +0100

    Modified hero on home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,30 +16,24 @@ export default function Hero() {
   );
 
   const steps = [
-    <span key="step1" className="flex items-center justify-center">
-      WE&rsquo;RE
+    <span
+      key="step1"
+      className="flex text-[8vw] sm:text-[6vw] md:text-[4vw] items-center justify-center"
+    >
+      WE&rsquo;RE MORE THAN A CAR WASH...
     </span>,
-    <span key="step1" className="flex items-center justify-center">
-      M{logo}RE
-    </span>,
-    <span key="step2" className="flex items-center justify-center">
-      TH{logo}N
-    </span>,
-    <span key="step3" className="flex items-center justify-center">
-      A
-    </span>,
-    <span key="step4" className="flex items-center justify-center">
-      C{logo}R WASH...
-    </span>,
-    <span key="step5" className="flex items-center justify-center">
-      WE&rsquo;RE DET{logo}ILERS!
+    <span
+      key="step5"
+      className="flex text-[10vw] sm:text-[8vw] md:text-[6vw] items-center justify-center"
+    >
+      WE&rsquo;RE DETAILERS!
     </span>,
   ];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setStepIndex((prev) => (prev + 1) % steps.length);
-    }, 1500);
+    }, 2000);
     return () => clearInterval(interval);
   }, []);
 
@@ -65,7 +59,7 @@ export default function Hero() {
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -10 }}
               transition={{ duration: 0.6 }}
-              className="text-[10vw] sm:text-[8vw] md:text-[6vw] font-bold leading-none text-glow-animated"
+              className="font-bold leading-none text-glow-animated"
             >
               {steps[stepIndex]}
             </motion.h1>
